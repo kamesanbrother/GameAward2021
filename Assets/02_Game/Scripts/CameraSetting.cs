697a6287efbfe6372f656084262a2ecd57a1ac77
Fernard <56015961+Do-GyeongGyu@users.noreply.github.com>
2021-04-22 13:13:45 +0900

    Project Setting

---
+++ Assets/02_Game/Scripts/CameraSetting.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSetting : MonoBehaviour
+{
+    private GameObject Main;
+    private GameObject Sub;
+    private bool SubCheck = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Main = GameObject.Find("Main Camera");
+        Sub = GameObject.Find("Sub Camera");
+
+        Sub.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SubCheck == false && Input.GetKeyDown(KeyCode.Z))
+        {
+            //サブカメラをアクティブに設定
+            Main.SetActive(false);
+            Sub.SetActive(true);
+            SubCheck = true;
+        }
+        else if (SubCheck == true && Input.GetKeyDown(KeyCode.Z))
+        {
+            //メインカメラをアクティブに設定
+            Main.SetActive(true);
+            Sub.SetActive(false);
+            SubCheck = false;
+        }
+    }
+}
