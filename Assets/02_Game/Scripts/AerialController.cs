1a3d010383af266065ada7d0d39f1e25907d1b59
kamesanbrother <kaito.s.990617@au.com>
2021-04-22 09:57:22 +0900

    Alpa Test Package

---
+++ Assets/02_Game/Scripts/AerialController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AerialController : MonoBehaviour
+{
+    private Transform ChangeWorld;
+    private Vector3 CheckPos = Vector3.zero;
+    private Vector3 Moving = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeWorld = this.transform;
+        CheckPos = transform.position;
+        Moving = new Vector3(CheckPos.x, CheckPos.y, 0.0f);
+
+        if(CheckPos.z == 3.5f && Input.GetKeyDown(KeyCode.C))
+        {
+            Moving.z = 0.0f;
+            ChangeWorld.position = Moving;
+        } 
+        else if(CheckPos.z == 0.0f && Input.GetKeyDown(KeyCode.C))
+        {
+            Moving.z = 3.5f;
+            ChangeWorld.position = Moving;
+        }
+    }
+}
---
+++ Assets/02_Game/Scripts/PlayerController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+        /* 操作方法
+    
+        移動      - 十字キー左右
+        ジャンプ  - SPACE
+    */
+
+    // パラメータ
+    public float Speed     = 5.0f;     // キャラクターの移動速度
+    public float JumpSpeed = 10.0f;    // ジャンプ力
+    public float Gravity   = 20.0f;    // 重力の大きさ
+
+    private CharacterController Controller;        // コンポーネントの取得
+    private Vector3 MoveDirection = Vector3.zero;  // キャラクターの移動量
+    private float h;                                // キー入力取得用
+    private Vector3 CurrentPos = Vector3.zero;
+    private Vector3 PosChange = Vector3.zero;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // CharactorControllerのコンポーネントを取得
+        Controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CurrentPos = this.transform.position;
+        PosChange = new Vector3(CurrentPos.x, CurrentPos.y, 0.0f);
+        // キー入力取得
+        h = Input.GetAxis("Horizontal");    // 値の範囲(-1.0f~1.0f)
+
+        // キャラクターの移動
+        if (Controller.isGrounded)// キャラクターが地面についているとき
+        {
+            MoveDirection = new Vector3(h, 0.0f, 0.0f);                    // キー入力でx成分のみ移動量に加える
+            MoveDirection = transform.TransformDirection(MoveDirection);  // キャラクターの移動に慣性をかける
+            MoveDirection *= Speed;                                       // キャラクターの設定スピードを乗算
+
+            if((CurrentPos.z == -1.0f) && Input.GetKey(KeyCode.Z))
+            {
+                PosChange.z = 1.5f;
+                //Controller.transform = PosChange;
+
+               // PosChange.z = 1.5f;
+                //CurrentPos.position = PosChange;
+            }
+            else if((CurrentPos.z == 1.5f) && Input.GetKey(KeyCode.Z))
+            {
+                PosChange.z = -1.0f;
+                Controller.Move(PosChange);
+
+                // PosChange.z = -1.0f;
+                // CurrentPos.position = PosChange;
+            }    
+
+            // ジャンプ
+            if(Input.GetKey(KeyCode.Space))
+            {
+                MoveDirection.y = JumpSpeed;  // y成分にキャラクターのジャンプ力を加算
+            }
+        }
+
+        // 重力設定
+        MoveDirection.y -= Gravity * Time.deltaTime;
+        Controller.Move(MoveDirection * Time.deltaTime);
+
+    }//Update
+
+}
